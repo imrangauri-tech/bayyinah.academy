46187fddf50adc02fe5268f830215791c319906a
imrangauri-tech <infra.reg@arabicjuniors.com>
2025-09-02 20:21:21 +0530

    Add Google Tag Manager (GTM-TTXQGS8H) to <head> in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TTXQGS8H');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${montserrat.variable} antialiased`}>
         <MainHeader />
         <main>{children}</main>
